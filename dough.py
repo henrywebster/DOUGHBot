3abef04410f14a759e623851131f8f0ea8ab52ea
Henry Webster <hwebster@mail.depaul.edu>
2017-09-12 14:52:08 -0500

    Doc'd more code, added to free toppings list and modifiers, added slick functional code to pizza generation

--- dough.py
+++ dough.py
@@ -1,8 +1,11 @@
 import random
 import math
+from fractions import Fraction
 
 MAX_PREMIUM = 3
 MAX_FREE = 4
+
+MOD_CHANCE = 2
 
 def fileToList (filename):
     """
@@ -10,8 +13,12 @@ def fileToList (filename):
     """
     return list (filter (None, (line.rstrip() for line in open(filename))))
 
+def randomPairSet (optionPair, size):
+    return list (map (
+        lambda x: ('', x) if random.randint(0, 2) else (random.choice(optionPair[1]), x), set (
+            map (lambda _: random.choice(optionPair[0]), [None] * size))))
 
-def generatePizza (premiumPair, freePair, frequency, seed):
+def generatePizza (premiumPair, freePair, seed):
     """
     premiumPair: tuple that represents a list of premium
     ingredients and a list of their possible modifiers
@@ -19,8 +26,6 @@ def generatePizza (premiumPair, freePair, frequency, seed):
     freePair: tuple that is a list of free ingredients
     and a list of their possible modifiers
     
-    frequency: the chance an ingredient will get a modifier
-
     seed: used for initializing random values
     """
 
@@ -29,19 +34,33 @@ def generatePizza (premiumPair, freePair, frequency, seed):
     # TODO: study the statistics of throwing out repeated vs 1 / 3
     # add verify stats & script for alphabetizing txt files
 
-    freeNum = random.randint (1, MAX_FREE)
-
-    pizza = ([],[])
-
+#    [None] * random.randint(1, MAX_PREMIUM)
+    
+    # premium
+    """
     for _ in range (0, random.randint (1, MAX_PREMIUM)):
         ingredient = random.choice (premiumPair[0])
         if (('', ingredient) not in pizza[0]):
             pizza[0].append (('',ingredient))       
+    """
 
-    for i, ingredient in enumerate(pizza[0]):
-        if (random.randint(0, math.floor(1 / frequency))):
-            pizza[0][i] = (random.choice(premiumPair[1]), ingredient[1])
-            
+    
+    pizza = (randomPairSet(
+        premiumPair, random.randint(1, MAX_PREMIUM)), randomPairSet(
+            freePair, random.randint(1, MAX_FREE)))
+
+    
+    """
+    # free
+    for _ in range (0, random.randint (1, MAX_FREE)):
+        ingredient = random.choice (freePair[0])
+        if (('', ingredient) not in pizza[1]):
+            pizza[1].append (('', ingredient))
+
+    for i, ingredient in enumerate(pizza[1]):
+        if (not random.randint(0, 2)):
+            pizza[1][i] = (random.choice(freePair[1]), ingredient[1])
+       """     
     return pizza
 
 
@@ -52,9 +71,23 @@ def main ():
     freeList = fileToList ('free.txt')
     freeModList = fileToList ('freemod.txt')
 
-    pizza = generatePizza ((premiumList,premiumModList), (freeList,freeModList), 0.5, 101212)
+    pizza = generatePizza ((premiumList,premiumModList), (freeList,freeModList), 101212)
+
+    # use a FOLD
     print (pizza)
-    
+    """
+    pizzaString = ""
+    if 2 == len(pizza[0][0]):
+        pizzaString = " and ".join(pizza[0][0])
+
+    elif len(pizza[0][0]) > 2:
+        pizzaString = ", ".join(pizza[0][0])
+        lIndex = pizzaString.rfind(',') + 1
+        pizzaString = pizzaString[:lIndex] + " and" + pizzaString[lIndex:]
+
+    else:
+        pizzaString = pizza[0]
+    """
 main()
 
     
--- sortlists.sh
+++ sortlists.sh
@@ -1,6 +1,11 @@
 
-lists=(premium.txt premiummod.txt free.txt freemod.txt)
 
+# Lint the text files by removing blank lines and putting
+# items in alphabetical order.
+
+# For human organizational purposes only; program does not care.
+
+lists=(premium.txt premiummod.txt free.txt freemod.txt)
 
 for list in "${lists[@]}"
 do
