3e6d6ce2144c078ff894e32d58d7399185b131c3
henry webster <hwebster@mail.depaul.edu>
2017-10-10 11:33:18 -0500

    Premium ingredients show up on image generation now. Added premium images and pepperoni ingredient.

--- oven.py
+++ oven.py
@@ -1,34 +1,47 @@
+"""Turns a pizza tupple into an image"""
+
 import json
 from PIL import Image, ImageDraw
 
 
-import dough
-
 MARKET_DIR = "recipes/"
-_IMAGE_DICT = {}
+_IMAGE_DICT = {"premium": {}, "free": {}}
+
+# TODO
+# * Add pizza background image instead of drawing natively.
+# * Load multiple recipes and vary size according to data. Define scale in JSON?
 
 
 def stock_kitchen():
     """
     Fill up the internal dictionaries with all the images from the markets.
     """
-    with open("recipes/basic/recipe.json") as f:
-        ingredientdict = json.load(f)
+    with open("recipes/basic/recipe.json") as _f:
+        ingredientdict = json.load(_f)
+
+
+    # TODO
+    # * define one NULL image that is shared
+    # * REFACTOR this
+    for ingredient, imagefile in ingredientdict["category"]["premium"].items():
+        if imagefile:
+            _IMAGE_DICT["premium"][ingredient] = Image.open("recipes/basic/" + imagefile)
+        else:
+            _IMAGE_DICT["premium"][ingredient] = Image.new("RGBA", (64, 64))
 
     for ingredient, imagefile in ingredientdict["category"]["free"].items():
         if imagefile:
-            _IMAGE_DICT[ingredient] = Image.open("recipes/basic/" + imagefile)
+            _IMAGE_DICT["free"][ingredient] = Image.open("recipes/basic/" + imagefile)
         else:
-            _IMAGE_DICT[ingredient] = Image.new("RGBA", (64, 64))
-
+            _IMAGE_DICT["free"][ingredient] = Image.new("RGBA", (64, 64))
 
 def bake_pizza(pizza):
     """
-	Create an image of the input pizza
+        Create an image of the input pizza
 
-	TODO: THESE VARIABLE NAMES / ORGANIZATION OF THIS IS TERRIBLE
+        TODO: VARIABLE NAMES / ORGANIZATION OF THIS IS TERRIBLE
 
-	REFACTOR REFACTOR REFACTOR
+        REFACTOR REFACTOR REFACTOR
     """
 
     # blank image
@@ -44,20 +57,24 @@ def bake_pizza(pizza):
     piz = Image.alpha_composite(piz, tomato)
 
     # mask for circu-lating the pizza
-    mask = _createPizzaMask(64, 64)
+    mask = _create_pizza_mask(64, 64)
 
     # add toppings
     pizzaimg = Image.new("RGBA", (64, 64))
 
+    for ingredient in pizza[0]:
+        pizzaimg = Image.alpha_composite(pizzaimg, _IMAGE_DICT["premium"][ingredient[1]])
+
     for ingredient in pizza[1]:
-        pizzaimg = Image.alpha_composite(pizzaimg, _IMAGE_DICT[ingredient[1]])
+        pizzaimg = Image.alpha_composite(pizzaimg, _IMAGE_DICT["free"][ingredient[1]])
 
     pizzaimg = Image.alpha_composite(piz, pizzaimg)
     pizzaimg = Image.composite(pizzaimg, background, mask)
 
     return pizzaimg.resize((64 * 5, 64 * 5), Image.NEAREST)
 
-def _createPizzaMask(width, height):
+
+def _create_pizza_mask(width, height):
     """
     Creates an ellipse of the needed size for an image mask to "carve out" pizza
     """
