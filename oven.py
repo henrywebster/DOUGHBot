cd614bca3681ba1aa3644c652e3e175a7c4b7416
henry webster <hwebster@mail.depaul.edu>
2017-10-10 00:53:37 -0500

    Improved crust color. Moved parts out of oven module

--- oven.py
+++ oven.py
@@ -1,55 +1,51 @@
 import json
 from PIL import Image, ImageDraw
-import functools
+
 
 import dough
 
 MARKET_DIR = "recipes/"
+_IMAGE_DICT = {}
 
 
-def stockKitchen():
+def stock_kitchen():
     """
     Fill up the internal dictionaries with all the images from the markets.
     """
-
-
-def bakePizza(pizza, market):
-    ingredientDict = {}
-
-    imageDict = {}
-
-
     with open("recipes/basic/recipe.json") as f:
-        ingredientDict = json.load(f)
+        ingredientdict = json.load(f)
 
-    for ingredient, imageFile in ingredientDict["category"]["free"].items():
-        if imageFile:
-            imageDict[ingredient] = Image.open("recipes/basic/" + imageFile)
+    for ingredient, imagefile in ingredientdict["category"]["free"].items():
+        if imagefile:
+            _IMAGE_DICT[ingredient] = Image.open("recipes/basic/" + imagefile)
+        else:
+            _IMAGE_DICT[ingredient] = Image.new("RGBA", (64, 64))
 
-        # blank image
+
+def bake_pizza(pizza):
+    """
+	Create an image of the input pizza
+    """
+
+    # blank image
     background = Image.new("RGBA", (64, 64))
 
     # image of pizza crust
-    piz = Image.new("RGBA", (64, 64), "orange")
+    piz = Image.new("RGBA", (64, 64), 0xff52c1ea)
 
     # mask for circu-lating the pizza
     mask = _createPizzaMask(64, 64)
 
     # add toppings
-    pizzaImg = Image.new("RGBA", (64, 64))
+    pizzaimg = Image.new("RGBA", (64, 64))
 
     for ingredient in pizza[1]:
-        pizzaImg = Image.alpha_composite(pizzaImg, imageDict[ingredient[1]])
+        pizzaimg = Image.alpha_composite(pizzaimg, _IMAGE_DICT[ingredient[1]])
 
-        # put the toppings on the pizza
-    pizzaImg = Image.alpha_composite(piz, pizzaImg)
+    pizzaimg = Image.alpha_composite(piz, pizzaimg)
+    pizzaimg = Image.composite(pizzaimg, background, mask)
 
-    # put the pizza on the background
-    pizzaImg = Image.composite(pizzaImg, background, mask)
-
-    # scale up and display
-    pizzaImg.resize((64 * 5, 64 * 5), Image.NEAREST).show()
-
+    return pizzaimg.resize((64 * 5, 64 * 5), Image.NEAREST)
 
 def _createPizzaMask(width, height):
     """
@@ -61,6 +57,3 @@ def _createPizzaMask(width, height):
     draw.ellipse([0, 0, width, height], fill="white")
 
     return mask
-
-pizza = dough.generatePizza(100)
-bakePizza(pizza, None)
