63af3030b6653e2bc836d0b9c7b29719b021f08c
henry webster <hwebster@mail.depaul.edu>
2017-10-10 10:24:48 -0500

    Reformatting, simplifying interface

--- dough.py
+++ dough.py
@@ -1,88 +1,74 @@
+"""underlying pizza-generation module of DOUGH system"""
+
 import random
-import sys
-from fractions import Fraction
 from functools import reduce
 
-VERSION_MAJOR = 0
-VERSION_MINOR = 3
+_MAX_PREMIUM = 3
+_MAX_FREE = 4
 
-MAX_PREMIUM = 3
-MAX_FREE = 4
 
-"""
-	(c) 2017 Henry Webster
+def _file_to_list(filename):
+    """turn a file with one item to a line into a python list"""
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    return list(filter(None, (line.rstrip() for line in open(filename))))
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-	
-# Helper functions for generatePizza
+# TODO: clean this up
+PREMIUM_LIST = _file_to_list('premium.txt')
+PREMIUM_MOD_LIST = _file_to_list('premiummod.txt')
+FREE_LIST = _file_to_list('free.txt')
+FREE_MOD_LIST = _file_to_list('freemod.txt')
 
-def fileToList (filename):
-    """
-    turn a file with one item to a line into a python list
-    """
-    return list (filter (None, (line.rstrip() for line in open(filename))))
 
-def generatePairList (optionPair, size):
+def _generate_pair_list(option_pair, size):
     """
     Return a list of tuples length of the size param. The tuples represent
-    (MODIFIER, TOPPING) pairs. There can be no duplicate topping component 
+    (MODIFIER, TOPPING) pairs. There can be no duplicate topping component
     in the tuple list, but there can be duplicate modifiers. The modifier
     component is optional.
     """
 
-	# The set() cast here might be the cause of the non-deterministic ordering
-	# for indgredients but this is low-priority.
-    return list (map (
-        lambda x: ('', x) if random.randint(0, 2) else (
-            random.choice(optionPair[1]), x), set (
-            map (lambda _: random.choice(optionPair[0]), [None] * size))))
+    # The set() cast here might be the cause of the non-deterministic ordering
+    # for indgredients but this is low-priority.
+    return list(map(
+        lambda x: ('', x) if random.randint(0, 2) else(
+            random.choice(option_pair[1]), x), set(
+            map(lambda _: random.choice(option_pair[0]), [None] * size))))
 
-def listToSentance (lst):
+
+def list_to_sentence(lst):
     """
-    Return the (MODIFIER, INGREDIENT) tuple list folded into a 
+    Return the (MODIFIER, INGREDIENT) tuple list folded into a
     comma-deliniated string.
 
     [('extra', 'tofu'), ('', 'ham')] => 'extra tofu, ham'
+
+    lst: (MODIFER, INGREDIENT) tuple list
+
+    returns: string in documented format
     """
 
-    return str(reduce (lambda x, y: "{}, {}".format(x, y), map(
+    return str(reduce(lambda x, y: "{}, {}".format(x, y), map(
         lambda x: "{} {}".format(x[0], x[1]) if x[0] else x[1], lst)))
-    
-def generatePizzaWithPairs (premiumPair, freePair, seed):
-    """
-    premiumPair: tuple that represents a list of premium
-    ingredients and a list of their possible modifiers
 
-    freePair: tuple that is a list of free ingredients
-    and a list of their possible modifiers
-    
+
+def generate_pizza(seed=None,
+                   premium_pair=(PREMIUM_LIST, PREMIUM_MOD_LIST),
+                   free_pair=(FREE_LIST, FREE_MOD_LIST)):
+    """
+    Create a pizza tuple based on input ingredients and RNG seed.
+
+    premium_pair: tuple of premium ingredients and their possible modifiers
+    free_pair: tuple of free ingredients and their possible modifiers
     seed: used for initializing random values
+
+    returns: pizza tuple
     """
 
     random.seed(seed)
 
-    pizza = (generatePairList(
-        premiumPair, random.randint(1, MAX_PREMIUM)), generatePairList(
-            freePair, random.randint(1, MAX_FREE)))
-    
+    pizza = (_generate_pair_list(
+        premium_pair, random.randint(1, _MAX_PREMIUM)), _generate_pair_list(
+            free_pair, random.randint(1, _MAX_FREE)))
+
     return pizza
-
-def generatePizza (seed):
-	return generatePizzaWithPairs((PREMIUM_LIST, PREMIUM_MOD_LIST), (FREE_LIST, FREE_MOD_LIST), seed)
-
-PREMIUM_LIST = fileToList ('premium.txt')
-PREMIUM_MOD_LIST = fileToList ('premiummod.txt')
-FREE_LIST = fileToList ('free.txt')
-FREE_MOD_LIST = fileToList ('freemod.txt')
