31e2bfb988f9f518d8765a553d23525ca4381193
henry webster <hwebster@mail.depaul.edu>
2017-10-13 23:32:18 -0500

    Added DOUGH class and untested generator functions

--- dough.py
+++ dough.py
@@ -7,17 +7,67 @@ from functools import reduce
 _MAX_PREMIUM = 3
 _MAX_FREE = 4
 
+# Might have gone overboard with these
 Category = namedtuple("Category", "modifiers, toppings")
 ToppingPair = namedtuple("ToppingPair", "modifier, topping")
+Pizza = namedtuple("Pizza", "premium, free")
 
 # TODO
 # * use list comprehensions instead of functional to be COOL and PYTHONIC
+# * implement Generator
 
 
 def _file_to_list(filename):
     """turn a file with one item to a line into a python list"""
 
     return [line.rstrip() for line in open(filename) if not None]
+
+
+class Dough():
+    """
+    D.O.U.G.H. stands for Digitally Optimized Utility for inGredient Hacking.
+    It will generate random pizzas based on an initial set of ingredients.
+    """
+
+    def __init__(self, 
+                 premium, 
+                 premiummod, 
+                 free, 
+                 freemod, 
+                 responses):
+
+        self.premiumpair = Category(premium, premiummod)
+        self.freepair = Category(free, freemod)
+        self.responses = responses
+
+    def generate_pizza(self, 
+                       seed=None):
+        """
+        Create a pizza tuple based on input ingredients and RNG seed.
+        """
+        random.seed(seed)
+
+        pizza = Pizza((_generate_pair_list(
+            self.premiumpair, random.randint(
+                1, _MAX_PREMIUM)), _generate_pair_list(
+                    self.freepair, random.randint(1, _MAX_FREE))))
+
+        yield pizza
+
+    def generate_response(self,
+                          pizza,
+                          name,
+                          seed=None):
+        """Create a random string response"""
+
+        # TODO
+        # * fill out docstring
+        # * figure out coupling & dangerous default warning
+        random.seed(seed)
+
+        return random.choice(self.responses).format(name,
+                                                    _list_to_sentence(pizza.premium),
+                                                    _list_to_sentence(pizza.free))
 
 
 # TODO
@@ -29,7 +79,8 @@ FREE_MOD_LIST = _file_to_list("freemod.txt")
 RESPONSE_LIST = _file_to_list("responses.txt")
 
 
-def _generate_pair_list(category, size):
+def _generate_pair_list(category, 
+                        size):
     """
     Return a list of tuples length of the size param. The tuples represent
     (MODIFIER, TOPPING) pairs. There can be no duplicate topping component
@@ -61,41 +112,3 @@ def _list_to_sentence(tuplelist):
     # This functional / list comprehension hybrid is beautiful however
     return reduce("{}, {}".format, ["{} {}".format(
         modifier, ingredient) if modifier else ingredient for modifier, ingredient in tuplelist])
-
-
-def generate_response(pizza, name, seed=None, responses=RESPONSE_LIST):
-    """Create a random string response"""
-
-    # TODO
-    # * fill out docstring
-    # * figure out coupling & dangerous default warning
-    random.seed(seed)
-
-    return random.choice(responses).format(name,
-                                           _list_to_sentence(pizza[0]),
-                                           _list_to_sentence(pizza[1]))
-
-
-def generate_pizza(seed=None,
-                   premium=PREMIUM_LIST,
-                   premiummod=PREMIUM_MOD_LIST,
-                   free=FREE_LIST,
-                   freemod=FREE_MOD_LIST):
-    """
-    Create a pizza tuple based on input ingredients and RNG seed.
-
-    premium_pair: tuple of premium ingredients and their possible modifiers
-    free_pair: tuple of free ingredients and their possible modifiers
-    seed: used for initializing random values
-
-    returns: pizza tuple
-    """
-
-    random.seed(seed)
-
-    pizza = (_generate_pair_list(
-        Category(premium, premiummod), random.randint(
-            1, _MAX_PREMIUM)), _generate_pair_list(
-                Category(free, freemod), random.randint(1, _MAX_FREE)))
-
-    return pizza
